c1283a4331ce2a837b9e43dc86a6e0767ca67c8c
Jenny <jennyngo.dev@gmail.com>
2019-04-10 17:00:04 -0400

    save message to database, render messages

--- src/components/AuthView/AuthView.js
+++ src/components/AuthView/AuthView.js
@@ -15,6 +15,25 @@ class AuthView extends Component {
     }
   }
 
+  componentDidMount() {
+    if(!this.state.chatroomData) {
+      this.showNewestRoom();
+      return;
+    }
+  }
+
+  showNewestRoom = () => {
+    const chatroomRef = db.collection('chatrooms')
+      chatroomRef.orderBy('created', 'desc').limit(1).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const newestRoomData =  doc.data();
+          this.setState({
+            activeChatroomData: newestRoomData
+          })
+        })
+      })
+  }
+
   showActiveChatroom = (chatroomId) => {
     const activeChatroomRef = db.collection('chatrooms').doc(chatroomId);
     activeChatroomRef.get().then((doc) => {
@@ -35,7 +54,7 @@ class AuthView extends Component {
         <Header text={'Chatroom'} />
         <Sidebar user={this.props.user} showActiveChatroom={this.showActiveChatroom} />
         {this.state.activeChatroomData &&
-          <Chatroom />
+          <Chatroom user={this.props.user} activeChatroomData={this.state.activeChatroomData} />
         }
       </main>
     )
--- src/components/Chatroom/Chatroom.js
+++ src/components/Chatroom/Chatroom.js
@@ -1,9 +1,90 @@
 import React, { Component } from 'react';
 
+import firebase, { db } from '../../firebase';
+
 class Chatroom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      draftedMessage: '',
+      chatroomData: props.activeChatroomData,
+      allMessages: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getAllMessages();
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log(prevProps, this.props);
+    if(prevProps.activeChatroomData.docId !== this.props.activeChatroomData.docId) {
+      this.setState({
+        chatroomData: this.props.activeChatroomData
+      })
+    }
+  }
+
+  getAllMessages = () => {
+    const { docId } = this.props.activeChatroomData;
+    const allMessagesRef = db.collection('chatrooms').doc(docId).collection('messages');
+    
+    allMessagesRef.orderBy('created', 'asc').onSnapshot((querySnapshot) => {
+      const messagesFromDatabaseArray = [];
+      querySnapshot.forEach((doc) => {
+        console.log('get all', doc.data())
+        messagesFromDatabaseArray.push(doc.data());
+      })
+
+      this.setState({
+        allMessages: messagesFromDatabaseArray
+      });
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      draftedMessage: e.target.value
+    })
+  }
+
+  saveMessageInDatabase = (e) => {
+    e.preventDefault();
+    const { docId } = this.props.activeChatroomData;
+    const messageRef = db.collection('chatrooms').doc(docId).collection('messages').doc();
+
+    const message = {
+      authorName: this.props.user.displayName,
+      authorUID: this.props.user.uid,
+      created: firebase.firestore.Timestamp.fromDate(new Date()),
+      message: this.state.draftedMessage,
+      messageId: messageRef.id
+    }
+    messageRef.set(message);
+    this.setState({
+      draftedMessage: ''
+    })
+  }
+
   render() {
     return (
-      <p>gonna be the chatrooom</p>
+      <div className="chatroom-window">
+        <div className="chatroom-window-messages">
+          {this.state.allMessages.map((doc) => {
+            return(
+              <div className="individual-message" key={doc.messageId}>
+                <span>{doc.authorName}</span>
+                <p>{doc.message}</p>
+              </div>
+            )
+          })}
+        </div>
+        <form onSubmit={this.saveMessageInDatabase}>
+          <label htmlFor="message">Enter message:</label>
+          <textarea onChange={this.handleChange} name="message" id="message" value={this.state.draftedMessage}></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     )
   }
 }
